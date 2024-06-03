d4118b2e8054038aa240254f3456255ccb05c6e9
bengal865 <69059209+bengal865@users.noreply.github.com>
2024-06-03 13:01:32 -0400

    Create python-strings-solution.py

---
+++ python-strings-solution.py
@@ -0,0 +1,10 @@
+# Python Strings SOLUTION
+
+# Concatenation
+first_name = 'Abraham'
+last_name = 'Lincoln'
+full_name = first_name + " " + last_name
+print(f'My first name is {first_name}.')
+print(f'My last name is {last_name}.')
+print(f'My full name is {full_name}.')
+
